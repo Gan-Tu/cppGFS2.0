569dd071176e3ccebb0323ee19427872c3b25d3a
Gan Tu <github@tugan.dev>
2020-04-28 00:17:56 -0700

    [chore] simplified linux package install changes

--- scripts/install_bazel.sh
+++ scripts/install_bazel.sh
@@ -1,26 +1,10 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-   sudo apt-get update
-   # Download unzip
-   sudo apt install unzip
-   if [[ $? != 0 ]]; then
-       echo "Failed to install unzip"
-       exit 1
-   fi
-
-   # Download gcc, g++
-   sudo apt install gcc
-   if [[ $? != 0 ]]; then
-       echo "Failed to install gcc"
-       exit 1
-   fi
-
-   sudo apt install g++
-   if [[ $? != 0 ]]; then
-       echo "Failed to install g++"
-       exit 1
-   fi
+# Download unzip, gcc, g++
+sudo apt-get update
+sudo apt install unzip gcc g++
+if [[ $? != 0 ]]; then
+   echo "Failed to install or upgrade essential packages"
+   exit 1
 fi
-
 # Download Linux installation script
 curl -fsSLO https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 if [[ $? != 0 ]]; then
@@ -34,6 +18,7 @@ if [[ $? != 0 ]]; then
     echo "Corrupted installer binary downloaded"
     exit 1
 fi
+# Install bazel on Linux
 mv bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh bazel-installer.sh
 chmod +x bazel-installer.sh
 sudo ./bazel-installer.sh
