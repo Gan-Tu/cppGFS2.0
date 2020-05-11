bcb6120d0bf5bcb5d48e60582335c59a65253cea
Gan Tu <github@tugan.dev>
2020-05-10 18:36:38 -0700

    [refactor] add more calls to mock client main

--- examples/run_mock_client_main.cc
+++ examples/run_mock_client_main.cc
@@ -7,7 +7,9 @@
 #include "src/protos/grpc/master_metadata_service.grpc.pb.h"
 
 using gfs::protocols::MasterMetadataServiceRpcProtocols;
+using google::protobuf::util::Status;
 using google::protobuf::util::StatusOr;
+using protos::grpc::DeleteFileRequest;
 using protos::grpc::OpenFileReply;
 using protos::grpc::OpenFileRequest;
 
@@ -20,17 +22,17 @@ int main(int argc, char** argv) {
   MasterMetadataServiceRpcProtocols metadata_client(master_channel);
 
   // Prepare a mock gRPC request and client context
-  OpenFileRequest request;
-  grpc::ClientContext client_context;
-  request.set_filename("/tmp/test_file");
-  request.set_chunk_index(0);
-  request.set_mode(OpenFileRequest::WRITE);
-  request.set_create_if_not_exists(true);
+  OpenFileRequest open_request;
+  grpc::ClientContext client_context1;
+  open_request.set_filename("/tmp/test_open_file");
+  open_request.set_chunk_index(0);
+  open_request.set_mode(OpenFileRequest::WRITE);
+  open_request.set_create_if_not_exists(true);
 
   // Send the request and print the response to screen
   StatusOr<OpenFileReply> status_or =
-      metadata_client.SendRequest(request, client_context);
-  std::cout << "Request sent: \n" << request.DebugString() << std::endl;
+      metadata_client.SendRequest(open_request, client_context1);
+  std::cout << "Request sent: \n" << open_request.DebugString() << std::endl;
 
   if (status_or.ok()) {
     std::cout << "Response received: \n"
@@ -39,6 +41,13 @@ int main(int argc, char** argv) {
     std::cout << "Request failed: \n"
               << status_or.status().ToString() << std::endl;
   }
+
+  DeleteFileRequest delete_request;
+  delete_request.set_filename("/tmp/test_delete_file");
+  grpc::ClientContext client_context2;
+  Status status = metadata_client.SendRequest(delete_request, client_context2);
+  std::cout << "Request sent: \n" << delete_request.DebugString() << std::endl;
+  std::cout << "Request status: \n" << status.ToString() << std::endl;
 
   return 0;
 }
