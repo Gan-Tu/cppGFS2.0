c10c0fc4c835c8f0fbe49fcac15748c3ea4ecee7
Xi Cheng <cx19871012@gmail.com>
2020-05-15 22:56:55 +0000

    [enhancement] Add the use example for the thread-safe parallel hash map

--- examples/parallel_hashmap_main.cc
+++ examples/parallel_hashmap_main.cc
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <thread>
+#include <vector>
 
+#include "absl/synchronization/mutex.h"
 #include "parallel_hashmap/phmap.h"
+
+// Define an intrinsically thread-safe flat hash map by parallel hashmap
+// The default definition assumes no internal lock and requires users
+// to pragmatically synchronize concurrent read and write to the parallel
+// hashmap. By passing a lock type, e.g. absl::Mutex, the parallel
+// hashmap is intrinsically thread safe. The example below follows the 
+// pattern defined in
+// https://github.com/greg7mdp/parallel-hashmap/blob/master/examples/bench.cc
+template <class Key, class Value>
+using thread_safe_flat_hash_map 
+    = phmap::parallel_flat_hash_map<
+        Key, 
+        Value,
+        phmap::container_internal::hash_default_hash<Key>,
+        phmap::container_internal::hash_default_eq<Key>,
+        std::allocator<std::pair<const Key, Value>>,
+        4,
+        absl::Mutex>;
 
 // Example Parallel Hashmap usage
 // https://github.com/greg7mdp/parallel-hashmap
 int main(int argc, char const* argv[]) {
+  // Default case, no internal lock protection.
   phmap::flat_hash_map<std::string, std::string> contacts = {
       {"tom", "tom@gmail.com"},
       {"jeff", "jk@gmail.com"},
@@ -16,6 +38,29 @@ int main(int argc, char const* argv[]) {
   }
   contacts["bill"] = "bg@whatever.com";
   std::cout << "bill's email is: " << contacts["bill"] << "\n";
+
+  thread_safe_flat_hash_map<std::string, std::string> thread_safe_contact;
+  int num_of_threads(20);
+
+  // Parallel insert. Note that using the default phmap::flat_hash_map you will run
+  // into race condition and likely encounter a core-dump with the following code
+  std::vector<std::thread> threads;
+  for (int i = 0; i < num_of_threads; i++) {
+    threads.push_back(std::thread([&, i]() {
+      std::string name(std::to_string(i));
+      std::string contact(name + "@gmail.com");
+      thread_safe_contact[name] = contact;
+    }));
+  }
+
+  for(int i = 0; i < num_of_threads; i++) {
+    threads[i].join();
+  }
+
+  for(int i = 0; i < num_of_threads; i++) {
+    std::string name(std::to_string(i));
+    std::cout << name << ", " << thread_safe_contact[name] << std::endl;
+  }
 
   return 0;
 }
