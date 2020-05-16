5862c7fee9cce1cbc7db92d735049d2fad3f5a0a
Gan Tu <github@tugan.dev>
2020-05-15 22:22:49 -0700

    [scripts] put more examples in the mock client

--- examples/run_mock_client_main.cc
+++ examples/run_mock_client_main.cc
@@ -15,15 +15,24 @@ using gfs::service::ChunkServerServiceClient;
 using gfs::service::MasterMetadataServiceClient;
 using google::protobuf::util::Status;
 using google::protobuf::util::StatusOr;
+using protos::grpc::AdvanceFileChunkVersionRequest;
 using protos::grpc::DeleteFileRequest;
-using protos::grpc::GrantLeaseReply;
 using protos::grpc::GrantLeaseRequest;
-using protos::grpc::InitFileChunkReply;
 using protos::grpc::InitFileChunkRequest;
-using protos::grpc::OpenFileReply;
 using protos::grpc::OpenFileRequest;
-using protos::grpc::RevokeLeaseReply;
+using protos::grpc::ReadFileChunkRequest;
 using protos::grpc::RevokeLeaseRequest;
+using protos::grpc::WriteFileChunkRequest;
+
+template <typename T, typename U>
+void LogRequestAndResponse(T request, StatusOr<U> reply_or) {
+  LOG(INFO) << "Request sent: \n" << request.DebugString();
+  if (reply_or.ok()) {
+    LOG(INFO) << "Response received: \n" << reply_or.ValueOrDie().DebugString();
+  } else {
+    LOG(ERROR) << "Request failed: \n" << reply_or.status().ToString();
+  }
+}
 
 int main(int argc, char** argv) {
   gfs::common::SystemLogger::GetInstance().Initialize(/*program_name=*/argv[0]);
@@ -39,87 +48,90 @@ int main(int argc, char** argv) {
   MasterMetadataServiceClient metadata_client(master_channel);
   ChunkServerServiceClient chunk_server_client(chunk_server_lease_channel);
 
-  // Prepare a mock gRPC request and client context
+  // Prepare a mock gRPC: OpenFile
   OpenFileRequest open_request;
-  grpc::ClientContext client_context1;
   open_request.set_filename("/tmp/test_open_file");
   open_request.set_chunk_index(0);
   open_request.set_mode(OpenFileRequest::WRITE);
   open_request.set_create_if_not_exists(true);
 
-  // Send the request and print the response to screen
-  StatusOr<OpenFileReply> metadata_or =
-      metadata_client.SendRequest(open_request, client_context1);
-  LOG(INFO) << "Request #1 sent: " << open_request.DebugString() << std::endl;
+  grpc::ClientContext client_context;
+  LogRequestAndResponse(
+      open_request, metadata_client.SendRequest(open_request, client_context));
 
-  if (metadata_or.ok()) {
-    LOG(INFO) << "Response #1 received: "
-              << metadata_or.ValueOrDie().DebugString() << std::endl;
-  } else {
-    LOG(INFO) << "Request #1 failed: " << metadata_or.status().ToString()
-              << std::endl;
-  }
-
+  // Prepare a mock gRPC: DeleteFile
   DeleteFileRequest delete_request;
   delete_request.set_filename("/tmp/test_delete_file");
+
   grpc::ClientContext client_context2;
   Status status = metadata_client.SendRequest(delete_request, client_context2);
-  LOG(INFO) << "Request #2 sent: " << delete_request.DebugString() << std::endl;
-  LOG(INFO) << "Request #2 status: " << status.ToString() << std::endl;
+  LOG(INFO) << "Request sent: " << delete_request.DebugString();
+  LOG(INFO) << "Request status: " << status.ToString();
 
+  // Prepare a mock gRPC: GrantLease
   google::protobuf::Timestamp lease_expiration_time;
   lease_expiration_time.set_seconds(1000);
   GrantLeaseRequest grant_lease_request;
   grant_lease_request.set_chunk_handle("/tmp/test_grant_lease");
   grant_lease_request.set_chunk_version(1);
   *grant_lease_request.mutable_lease_expiration_time() = lease_expiration_time;
+
   grpc::ClientContext client_context3;
-  StatusOr<GrantLeaseReply> grant_lease_or =
-      chunk_server_client.SendRequest(grant_lease_request, client_context3);
-  LOG(INFO) << "Request #3 sent: " << grant_lease_request.DebugString()
-            << std::endl;
+  LogRequestAndResponse(
+      grant_lease_request,
+      chunk_server_client.SendRequest(grant_lease_request, client_context3));
 
-  if (grant_lease_or.ok()) {
-    LOG(INFO) << "Response #3 received: "
-              << grant_lease_or.ValueOrDie().DebugString() << std::endl;
-  } else {
-    LOG(INFO) << "Request #3 failed: " << grant_lease_or.status().ToString()
-              << std::endl;
-  }
-
+  // Prepare a mock gRPC: RevokeLease
   RevokeLeaseRequest revoke_lease_request;
   revoke_lease_request.set_chunk_handle("/tmp/test_revoke_lease");
   *revoke_lease_request.mutable_original_lease_expiration_time() =
       lease_expiration_time;
+
   grpc::ClientContext client_context4;
-  StatusOr<RevokeLeaseReply> revoke_lease_or =
-      chunk_server_client.SendRequest(revoke_lease_request, client_context4);
-  LOG(INFO) << "Request #4 sent: " << revoke_lease_request.DebugString()
-            << std::endl;
+  LogRequestAndResponse(
+      revoke_lease_request,
+      chunk_server_client.SendRequest(revoke_lease_request, client_context4));
 
-  if (revoke_lease_or.ok()) {
-    LOG(INFO) << "Response #4 received: "
-              << revoke_lease_or.ValueOrDie().DebugString() << std::endl;
-  } else {
-    LOG(INFO) << "Request #4 failed: " << revoke_lease_or.status().ToString()
-              << std::endl;
-  }
-
+  // Prepare a mock gRPC: InitFileChunk
   InitFileChunkRequest init_file_request;
   init_file_request.set_chunk_handle("/tmp/test_init_file");
-  grpc::ClientContext client_context5;
-  StatusOr<InitFileChunkReply> init_file_or =
-      chunk_server_client.SendRequest(init_file_request, client_context5);
-  LOG(INFO) << "Request #4 sent: " << init_file_request.DebugString()
-            << std::endl;
 
-  if (init_file_or.ok()) {
-    LOG(INFO) << "Response #4 received: "
-              << init_file_or.ValueOrDie().DebugString() << std::endl;
-  } else {
-    LOG(INFO) << "Request #4 failed: " << init_file_or.status().ToString()
-              << std::endl;
-  }
+  grpc::ClientContext client_context5;
+  LogRequestAndResponse(
+      init_file_request,
+      chunk_server_client.SendRequest(init_file_request, client_context5));
+
+  // Prepare a mock gRPC: ReadFileChunk
+  ReadFileChunkRequest read_file_request;
+  read_file_request.set_chunk_handle("/tmp/test_read_file");
+
+  grpc::ClientContext client_context6;
+  LogRequestAndResponse(
+      read_file_request,
+      chunk_server_client.SendRequest(read_file_request, client_context6));
+
+  // Prepare a mock gRPC: WriteFileChunk
+  WriteFileChunkRequest write_file_request;
+  write_file_request.set_chunk_handle("/tmp/test_write_file");
+  write_file_request.set_chunk_version(10);
+  write_file_request.set_offset_start(100);
+  write_file_request.set_length(50);
+  std::string payload = "Hello World";
+  write_file_request.set_data(payload.c_str());
+
+  grpc::ClientContext client_context8;
+  LogRequestAndResponse(
+      write_file_request,
+      chunk_server_client.SendRequest(write_file_request, client_context8));
+
+  // Prepare a mock gRPC: AdvanceFileChunkVersion
+  AdvanceFileChunkVersionRequest advance_version_request;
+  advance_version_request.set_chunk_handle("/tmp/test_advance_version");
+
+  grpc::ClientContext client_context9;
+  LogRequestAndResponse(advance_version_request,
+                        chunk_server_client.SendRequest(advance_version_request,
+                                                        client_context9));
 
   return 0;
 }
