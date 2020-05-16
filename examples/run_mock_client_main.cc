e167f36fe77399c54a4963d1bcc85f9ce35c7771
Gan Tu <github@tugan.dev>
2020-05-15 17:18:46 -0700

    [feat] add example client to use lease client

--- examples/run_mock_client_main.cc
+++ examples/run_mock_client_main.cc
@@ -2,24 +2,37 @@
 #include <memory>
 #include <string>
 
+#include "google/protobuf/timestamp.pb.h"
 #include "grpcpp/grpcpp.h"
+#include "src/common/protocol_client/chunk_server_lease_service_client.h"
 #include "src/common/protocol_client/master_metadata_service_client.h"
+#include "src/protos/grpc/chunk_server_lease_service.grpc.pb.h"
 #include "src/protos/grpc/master_metadata_service.grpc.pb.h"
 
+using gfs::service::ChunkServerLeaseServiceClient;
 using gfs::service::MasterMetadataServiceClient;
 using google::protobuf::util::Status;
 using google::protobuf::util::StatusOr;
 using protos::grpc::DeleteFileRequest;
+using protos::grpc::GrantLeaseReply;
+using protos::grpc::GrantLeaseRequest;
 using protos::grpc::OpenFileReply;
 using protos::grpc::OpenFileRequest;
+using protos::grpc::RevokeLeaseReply;
+using protos::grpc::RevokeLeaseRequest;
 
 int main(int argc, char** argv) {
   // Initialize an instance of communication manager
   // TODO(tugan): add support to listen on host:port based on configuration
-  std::string server_address("0.0.0.0:50051");
+  std::string metadata_service_address("0.0.0.0:50051");
+  std::string lease_service_address("0.0.0.0:50052");
   auto credentials = grpc::InsecureChannelCredentials();
-  auto master_channel = grpc::CreateChannel(server_address, credentials);
+  auto master_channel =
+      grpc::CreateChannel(metadata_service_address, credentials);
+  auto chunk_server_lease_channel =
+      grpc::CreateChannel(lease_service_address, credentials);
   MasterMetadataServiceClient metadata_client(master_channel);
+  ChunkServerLeaseServiceClient lease_client(chunk_server_lease_channel);
 
   // Prepare a mock gRPC request and client context
   OpenFileRequest open_request;
@@ -30,16 +43,16 @@ int main(int argc, char** argv) {
   open_request.set_create_if_not_exists(true);
 
   // Send the request and print the response to screen
-  StatusOr<OpenFileReply> status_or =
+  StatusOr<OpenFileReply> metadata_or =
       metadata_client.SendRequest(open_request, client_context1);
   std::cout << "Request sent: \n" << open_request.DebugString() << std::endl;
 
-  if (status_or.ok()) {
+  if (metadata_or.ok()) {
     std::cout << "Response received: \n"
-              << status_or.ValueOrDie().DebugString() << std::endl;
+              << metadata_or.ValueOrDie().DebugString() << std::endl;
   } else {
     std::cout << "Request failed: \n"
-              << status_or.status().ToString() << std::endl;
+              << metadata_or.status().ToString() << std::endl;
   }
 
   DeleteFileRequest delete_request;
@@ -48,6 +61,27 @@ int main(int argc, char** argv) {
   Status status = metadata_client.SendRequest(delete_request, client_context2);
   std::cout << "Request sent: \n" << delete_request.DebugString() << std::endl;
   std::cout << "Request status: \n" << status.ToString() << std::endl;
+
+  google::protobuf::Timestamp lease_expiration_time;
+  lease_expiration_time.set_seconds(1000);
+
+  GrantLeaseRequest grant_lease_request;
+  grant_lease_request.set_chunk_handle("/tmp/test_grant_lease");
+  grant_lease_request.set_chunk_version(1);
+  *grant_lease_request.mutable_lease_expiration_time() = lease_expiration_time;
+  grpc::ClientContext client_context3;
+  StatusOr<GrantLeaseReply> grant_lease_or =
+      lease_client.SendRequest(grant_lease_request, client_context3);
+  std::cout << "Request sent: \n"
+            << grant_lease_request.DebugString() << std::endl;
+
+  if (grant_lease_or.ok()) {
+    std::cout << "Response received: \n"
+              << grant_lease_or.ValueOrDie().DebugString() << std::endl;
+  } else {
+    std::cout << "Request failed: \n"
+              << grant_lease_or.status().ToString() << std::endl;
+  }
 
   return 0;
 }
---
+++ src/server/chunk_server/run_chunk_server_main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "grpcpp/grpcpp.h"
+#include "src/server/chunk_server/chunk_server_lease_service_impl.h"
+
+using gfs::service::ChunkServerLeaseServiceImpl;
+using grpc::Server;
+using grpc::ServerBuilder;
+
+int main(int argc, char** argv) {
+  ServerBuilder builder;
+
+  // TODO(tugan): add support to listen on host:port based on configuration
+  std::string server_address("0.0.0.0:50052");
+
+  // Listen on the given address without any authentication mechanism for now.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+  // Register a synchronous service for handling clients' metadata requests
+  ChunkServerLeaseServiceImpl lease_service;
+  builder.RegisterService(&lease_service);
+
+  // Assemble and start the server
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+
+  return 0;
+}
