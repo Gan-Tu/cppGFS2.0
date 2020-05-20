a08559f491b2bf6fab0330187e0231c276b153de
Xi Cheng <cx19871012@gmail.com>
2020-05-20 07:02:41 +0000

    [feat] Cleanup comments in client's header and add OpenFlag enum type

--- src/client/gfs_client.cc
+++ src/client/gfs_client.cc
@@ -3,11 +3,22 @@
 namespace gfs {
 namespace client {
 
-google::protobuf::util::Status remove(const char* pathname) {
-  return google::protobuf::util::Status::OK;
-}
+google::protobuf::util::Status open(const char* pathname, unsigned int flags) {
+  // Step 1. Check and validate the flags, e.g. it doesn't make sense to 
+  // open with both read and write flags
 
-google::protobuf::util::Status open(const char* pathname, int flags) {
+  // Creation mode
+  if (flags | OpenFlag::Create) {
+  }
+
+  // Read mode
+  if (flags | OpenFlag::Read) {
+  }
+
+  // Write mode
+  if (flags | OpenFlag::Read) {
+  }
+
   return google::protobuf::util::Status::OK;
 }
 
@@ -22,6 +33,10 @@ google::protobuf::util::StatusOr<Data> read(const char* pathname, size_t offset,
 
 google::protobuf::util::Status write(const char* path, void* buffer,
                                      size_t offset, size_t nbytes) {
+  return google::protobuf::util::Status::OK;
+}
+
+google::protobuf::util::Status remove(const char* pathname) {
   return google::protobuf::util::Status::OK;
 }
 
