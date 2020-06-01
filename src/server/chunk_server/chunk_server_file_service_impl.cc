df1b663c4d44a9e8b262f1bec6d18fc828aa7be4
Gan Tu <github@tugan.dev>
2020-05-31 23:13:30 -0700

    [feat] add initial server logic for InitFileChunk

--- src/server/chunk_server/chunk_server_file_service_impl.cc
+++ src/server/chunk_server/chunk_server_file_service_impl.cc
@@ -1,8 +1,13 @@
 #include "src/server/chunk_server/chunk_server_file_service_impl.h"
 
 #include "grpcpp/grpcpp.h"
+#include "src/common/system_logger.h"
 #include "src/protos/grpc/chunk_server_file_service.grpc.pb.h"
 
+using gfs::common::utils::ConvertProtobufStatusToGrpcStatus;
+using google::protobuf::util::Status;
+using google::protobuf::util::StatusOr;
+using StatusCode = google::protobuf::util::error::Code;
 using grpc::ServerContext;
 using protos::grpc::AdvanceFileChunkVersionReply;
 using protos::grpc::AdvanceFileChunkVersionRequest;
@@ -23,10 +28,31 @@ namespace service {
 grpc::Status ChunkServerFileServiceImpl::InitFileChunk(
     ServerContext* context, const InitFileChunkRequest* request,
     InitFileChunkReply* reply) {
-  // TODO(someone): implement the GFS chunk server logic here
+  LOG(INFO) << "Received InitFileChunkRequest:" << (*request).DebugString();
   *reply->mutable_request() = *request;
-  reply->set_status(InitFileChunkReply::ALREADY_EXISTS);
-  return grpc::Status::OK;
+
+  LOG(INFO) << "Preparing redo logs for InitFileChunk of file handle: "
+            << request->chunk_handle();
+  // TODO: may not be necessary depending on how crash-resistent we want to be
+  LOG(INFO) << "InitFileChunk redo logs prepared: " << request->chunk_handle();
+
+  LOG(INFO) << "Trying to create the file chunk with initial version of 1: "
+            << request->chunk_handle();
+  Status status = file_manager_->CreateChunk(request->chunk_handle(), 1);
+  if (status.ok()) {
+    LOG(INFO) << "Initial empty file chunk successfully created: "
+              << request->chunk_handle();
+    reply->set_status(InitFileChunkReply::CREATED);
+    return grpc::Status::OK;
+  } else if (status.error_code() == StatusCode::ALREADY_EXISTS) {
+    LOG(ERROR) << "Cannot initialize file chunk because it already exits: "
+               << request->chunk_handle();
+    reply->set_status(InitFileChunkReply::ALREADY_EXISTS);
+    return grpc::Status::OK;
+  } else {
+    LOG(ERROR) << "Unexpected error when initializing file chunk: " << status;
+    return ConvertProtobufStatusToGrpcStatus(status);
+  }
 }
 
 grpc::Status ChunkServerFileServiceImpl::ReadFileChunk(
