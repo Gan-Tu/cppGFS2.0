b0d530771bf2aff13bc661f948be4dfca8f65173
Xi Cheng <cx19871012@gmail.com>
2020-06-03 23:46:30 +0000

    [feat] Add HandleWrite in MasterMetadataServiceImpl with the advancement of chunk version and requesting lease

--- src/server/master_server/master_metadata_service_impl.cc
+++ src/server/master_server/master_metadata_service_impl.cc
@@ -1,5 +1,6 @@
 #include "src/server/master_server/master_metadata_service_impl.h"
 
+#include "absl/time/time.h"
 #include "grpcpp/grpcpp.h"
 #include "src/common/protocol_client/grpc_client_utils.h"
 #include "src/common/system_logger.h"
@@ -14,7 +15,11 @@ using google::protobuf::Empty;
 using grpc::ServerContext;
 using protos::ChunkServerLocation;
 using protos::FileChunkMetadata;
+using protos::grpc::AdvanceFileChunkVersionRequest;
+using protos::grpc::AdvanceFileChunkVersionReply;
 using protos::grpc::DeleteFileRequest;
+using protos::grpc::GrantLeaseRequest;
+using protos::grpc::GrantLeaseReply;
 using protos::grpc::InitFileChunkReply;
 using protos::grpc::InitFileChunkRequest;
 using protos::grpc::OpenFileReply;
@@ -189,9 +194,90 @@ grpc::Status MasterMetadataServiceImpl::HandleFileChunkWrite(
       metadata_manager()->GetChunkHandle(filename, chunk_index));
 
   if (!chunk_handle_or.ok()) {
+    if (!request->create_if_not_exists()) {
+      LOG(ERROR) << "create_if_not_exists not set when writing to file "
+                 << filename << " at chunk index " << chunk_index 
+                 << " but chunk does not exist";
+      return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, 
+                         "create_if_not_exists not set");
+    }
+     
+    LOG(INFO) << "Creating a file chunk for " << filename << " at chunk index "
+              << chunk_index;
+    grpc::Status chunk_creation_status(HandleFileChunkCreation(request, reply));
+    if (!chunk_creation_status.ok()) {
+      return chunk_creation_status;
+    }
   }
- 
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "needs implementation");
+
+  const std::string& chunk_handle(reply->metadata().chunk_handle());
+  uint32_t chunk_version(reply->metadata().version());
+  
+  // Now that all chunk server locations are stored in reply, the master first
+  // advances the chunk version of this chunk and then send a GrantLeaseRequest
+  for (auto chunk_server_location : reply->metadata().locations()) {
+    const std::string server_address(
+        chunk_server_location.server_hostname() + ":" +
+        std::to_string(chunk_server_location.server_port()));
+    // Create and return this chunk server Rpc client if not exist
+    auto chunk_server_service_client =
+        GetOrCreateChunkServerProtocolClient(server_address);
+    // Issue AdvanceFileChunkVersion request
+    AdvanceFileChunkVersionRequest advance_version_request;
+    advance_version_request.set_chunk_handle(chunk_handle);
+    // Advance the chunk version by 1
+    advance_version_request.set_new_chunk_version(chunk_version + 1);
+    grpc::ClientContext client_context;
+    common::SetClientContextDeadline(client_context, config_manager_);
+
+    // Issue AdvanceFileChunkVersion and check status
+    google::protobuf::util::StatusOr<AdvanceFileChunkVersionReply>
+        advance_version_reply_or(
+            chunk_server_service_client->SendRequest(advance_version_request,
+                                                     client_context));
+    if (!advance_version_reply_or.ok()) {
+      LOG(ERROR) << "Failed to advance chunk version for chunk " 
+                 << chunk_handle << " on chunk server " << server_address
+                 << "due to " << advance_version_reply_or.status();
+      // TODO(Xi): handle if all version advancement fails
+    } else {
+      LOG(INFO) << "Advanced chunk version for chunk " << chunk_handle
+                << " on chunk server " << server_address;
+    }
+  }
+
+  // After advancing the chunk version, obtain a lease
+  const std::string& primary_server_address(
+      reply->metadata().primary_location().server_hostname() + 
+      std::to_string(reply->metadata().primary_location().server_port()));
+  auto lease_service_client(GetOrCreateChunkServerProtocolClient(
+                                primary_server_address));
+  // Prepare GrantLeaseRequest to send to chunk server
+  GrantLeaseRequest grant_lease_request;
+  grant_lease_request.set_chunk_handle(chunk_handle);
+  grant_lease_request.set_chunk_version(chunk_version + 1);
+  grant_lease_request.mutable_lease_expiration_time()->set_seconds( 
+      absl::ToUnixSeconds(absl::Now() + 
+                              config_manager_->GetWriteLeaseTimeout()));
+  grpc::ClientContext client_context;
+  common::SetClientContextDeadline(client_context, config_manager_);
+
+  // Issue GrantLeaseRequest request and check status
+  google::protobuf::util::StatusOr<GrantLeaseReply> grant_lease_reply_or(
+      lease_service_client->SendRequest(grant_lease_request, client_context));
+
+  // Handle error, and logging
+  if (!grant_lease_reply_or.ok()) {
+    LOG(ERROR) << "Grant lease request for chunk " << chunk_handle 
+               << " at " << primary_server_address << " failed due to "
+               << grant_lease_reply_or.status();
+    return common::utils::ConvertProtobufStatusToGrpcStatus(
+               grant_lease_reply_or.status());
+  } else {
+    LOG(INFO) << "Grant lease request for chunk " << chunk_handle
+              << " at " << primary_server_address << " succeeded";
+  }
+  return grpc::Status::OK;
 }
 
 std::shared_ptr<ChunkServerServiceMasterServerClient>
