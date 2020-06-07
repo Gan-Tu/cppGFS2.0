d5bfc2caa736c235e1957f0a7d40d2fd79809a3d
Gan Tu <github@tugan.dev>
2020-06-07 02:39:28 -0700

    modify test to test the correct setup

--- tests/server/chunk_server/chunk_server_file_impl_asyc_test.cc
+++ tests/server/chunk_server/chunk_server_file_impl_asyc_test.cc
@@ -7,9 +7,11 @@
 #include "gtest/gtest.h"
 #include "src/client/gfs_client.h"
 #include "src/common/config_manager.h"
+#include "src/protos/chunk_server.pb.h"
 #include "src/server/chunk_server/chunk_server_file_service_impl.h"
 #include "src/server/chunk_server/chunk_server_impl.h"
 #include "src/server/chunk_server/file_chunk_manager.h"
+#include "src/server/master_server/chunk_server_manager.h"
 #include "src/server/master_server/master_metadata_service_impl.h"
 #include "src/server/master_server/metadata_manager.h"
 #include "tests/utils.h"
@@ -19,15 +21,15 @@ const std::string kTestConfigPath = "tests/server/chunk_server/test_config.yml";
 const std::string kTestChunkServerName = "chunk_server_02";
 const uint32_t kTestChunkServerPort = 50053;
 const std::string kTestChunkServerHostName = "0.0.0.0";
-const std::string kTestChunkServerAddress 
-    = kTestChunkServerHostName + ":" + std::to_string(kTestChunkServerPort);
+const std::string kTestChunkServerAddress =
+    kTestChunkServerHostName + ":" + std::to_string(kTestChunkServerPort);
 
 // Config info for a semi-mocked master server
 const uint32_t kTestMasterServerPort = 50051;
 const std::string kTestMasterServerHostName = "0.0.0.0";
 const std::string kTestMasterServerName = "master_server_01";
-const std::string kTestMasterServerAddress  
-    = kTestMasterServerHostName + ":" + std::to_string(kTestMasterServerPort);
+const std::string kTestMasterServerAddress =
+    kTestMasterServerHostName + ":" + std::to_string(kTestMasterServerPort);
 
 // Sample data used for testing
 const std::string kSmallDataFileName("/kSmallData");
@@ -37,10 +39,11 @@ const uint32_t kSmallDataFileChunkVersion(2);
 
 using gfs::common::ConfigManager;
 using gfs::server::ChunkServerImpl;
+using gfs::server::ChunkServerManager;
 using gfs::server::FileChunkManager;
 using gfs::server::MetadataManager;
-using gfs::service::MasterMetadataServiceImpl;
 using gfs::service::ChunkServerFileServiceImpl;
+using gfs::service::MasterMetadataServiceImpl;
 using google::protobuf::util::Status;
 using google::protobuf::util::StatusOr;
 using grpc::Server;
@@ -54,7 +57,7 @@ void SeedTestChunkData() {
   const uint32_t version(kSmallDataFileChunkVersion);
   // Create a chunk with version 2
   ASSERT_TRUE(file_chunk_manager->CreateChunk(chunk_handle, version).ok());
-  
+
   // Write to the chunk
   auto write_result = file_chunk_manager->WriteToChunk(
       chunk_handle, version, /*start_offset=*/0, kSmallData.size(), kSmallData);
@@ -69,17 +72,28 @@ void SeedTestMetadata() {
   ASSERT_TRUE(metadata_manager->CreateFileMetadata(kSmallDataFileName).ok());
   ASSERT_TRUE(metadata_manager->CreateChunkHandle(kSmallDataFileName, 0).ok());
   // Bump up the version to 2
-  ASSERT_TRUE(metadata_manager->AdvanceChunkVersion(kSmallDataFileChunkHandle)
-                  .ok());
-  ASSERT_TRUE(metadata_manager->AdvanceChunkVersion(kSmallDataFileChunkHandle)
-                  .ok());
+  ASSERT_TRUE(
+      metadata_manager->AdvanceChunkVersion(kSmallDataFileChunkHandle).ok());
+  ASSERT_TRUE(
+      metadata_manager->AdvanceChunkVersion(kSmallDataFileChunkHandle).ok());
   // Set the chunk server location to be the one used in this test
-  auto file_chunk_metadata(metadata_manager->GetFileChunkMetadata(
-                               kSmallDataFileChunkHandle).ValueOrDie());
+  auto file_chunk_metadata(
+      metadata_manager->GetFileChunkMetadata(kSmallDataFileChunkHandle)
+          .ValueOrDie());
   file_chunk_metadata.mutable_locations()->Add(
       tests::ChunkServerLocationBuilder(kTestChunkServerName,
                                         kTestChunkServerPort));
   metadata_manager->SetFileChunkMetadata(file_chunk_metadata);
+  protos::ChunkServer chunk_server;
+  chunk_server.mutable_location()->set_server_hostname(kTestChunkServerName);
+  chunk_server.mutable_location()->set_server_port(kTestChunkServerPort);
+  chunk_server.set_available_disk_mb(1000);
+  *chunk_server.add_stored_chunk_handles() = kSmallDataFileChunkHandle;
+  ASSERT_TRUE(ChunkServerManager::GetInstance().RegisterChunkServer(
+      std::make_shared<protos::ChunkServer>(chunk_server)));
+  ASSERT_TRUE(ChunkServerManager::GetInstance()
+                  .AllocateChunkServer(kSmallDataFileChunkHandle, 1)
+                  .size() > 0);
 }
 
 // Start a test chunk server, this gets called from a separate thread
@@ -95,7 +109,7 @@ void StartTestChunkServer() {
   ChunkServerFileServiceImpl file_service(chunk_server);
   builder.RegisterService(&file_service);
   // Initialize test data on the chunk server
-  SeedTestChunkData(); 
+  SeedTestChunkData();
   // Start the server, and let it run until thread is cancelled
   std::unique_ptr<Server> server(builder.BuildAndStart());
   server->Wait();
@@ -107,8 +121,8 @@ void StartTestMasterServer() {
   auto credentials = grpc::InsecureServerCredentials();
   builder.AddListeningPort(kTestMasterServerAddress, credentials);
   // Register the master metadata service impl
-  ConfigManager* config = ConfigManager::GetConfig(kTestConfigPath)
-                              .ValueOrDie();
+  ConfigManager* config =
+      ConfigManager::GetConfig(kTestConfigPath).ValueOrDie();
   MasterMetadataServiceImpl metadata_service(config);
   builder.RegisterService(&metadata_service);
   // Initialize test data on the master server
@@ -120,23 +134,23 @@ void StartTestMasterServer() {
 
 // Initialize the client impl and make it ready to call client function
 void StartClient() {
-  Status init_status(gfs::client::init_client(kTestConfigPath, 
-                                              "master_server_01", true));
+  Status init_status(
+      gfs::client::init_client(kTestConfigPath, "master_server_01", true));
   EXPECT_TRUE(init_status.ok());
 }
 
 // Helper funtion to perform a read request of a piece of data. It performs a
 // read request for the filename {data_filename}, at {offset} for {length}
-// of data.       
+// of data.
 void SingleClientReadData(const std::string& data_filename,
-                          const std::string& data, size_t offset, 
+                          const std::string& data, size_t offset,
                           size_t length) {
   size_t actual_read_bytes;
-  // Compute the actual bytes that we will read here. The reason of adding 
+  // Compute the actual bytes that we will read here. The reason of adding
   // an explicit check below is because data.size() and offset are unsigned int
   // and directly minus may result in overflow and incorrect result.
-  // One could use the following expression for short (less reasable though):
-  //   actual_read_bytes = offset >= data.size() ? 0 : 
+  // One could use the following expression for short (less readable though):
+  //   actual_read_bytes = offset >= data.size() ? 0 :
   //                           std::min(data.size() - offset, length);
   if (offset >= data.size()) {
     actual_read_bytes = 0;
@@ -149,8 +163,8 @@ void SingleClientReadData(const std::string& data_filename,
   // Make sure that the read data is expected
   auto read_data(read_result.ValueOrDie());
   EXPECT_EQ(read_data.bytes_read, actual_read_bytes);
-  EXPECT_EQ(memcmp(read_data.buffer, data.c_str() + offset, 
-                   read_data.bytes_read), 0);
+  EXPECT_EQ(
+      memcmp(read_data.buffer, data.c_str() + offset, read_data.bytes_read), 0);
   // Cleanup to prevent memory leak
   free(read_data.buffer);
 }
@@ -160,16 +174,15 @@ void SingleClientReadShortData() {
   // Read full length
   SingleClientReadData(kSmallDataFileName, kSmallData, 0, data_len);
   // Read second half length
-  SingleClientReadData(kSmallDataFileName, kSmallData, data_len / 2, 
-                       data_len - data_len / 2);  
+  SingleClientReadData(kSmallDataFileName, kSmallData, data_len / 2,
+                       data_len - data_len / 2);
   // Start from last quarter but try to read full length
   SingleClientReadData(kSmallDataFileName, kSmallData, data_len - data_len / 4,
                        data_len);
 }
 
-// TODO(Gan): refactor / rewrite this test for more cases 
-class ChunkServerFileServiceTest : public ::testing::Test {
-};
+// TODO(Gan): refactor / rewrite this test for more cases
+class ChunkServerFileServiceTest : public ::testing::Test {};
 
 // Test a single client makes a read request of small data
 TEST_F(ChunkServerFileServiceTest, SimpleClientReadShortDataTest) {
@@ -183,8 +196,8 @@ TEST_F(ChunkServerFileServiceTest, ConcurrentClientReadShortDataTest) {
   std::vector<std::thread> threads;
   for (int i = 0; i < num_of_threads; i++) {
     threads.push_back(std::thread([]() {
-        StartClient();
-        SingleClientReadShortData();
+      StartClient();
+      SingleClientReadShortData();
     }));
   }
 
@@ -194,13 +207,13 @@ TEST_F(ChunkServerFileServiceTest, ConcurrentClientReadShortDataTest) {
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  // Start a chunk server for file service handler in the background, 
+  // Start a chunk server for file service handler in the background
   std::thread chunk_server_thread = std::thread(StartTestChunkServer);
-  
+
   // Start a master server file metadata service handler in the background
   std::thread master_server_thread = std::thread(StartTestMasterServer);
 
-  // Wait some time for the server to successfully start in background
+  // Wait more time for the server to successfully start in background
   std::this_thread::sleep_for(std::chrono::seconds(3));
 
   // Run tests
