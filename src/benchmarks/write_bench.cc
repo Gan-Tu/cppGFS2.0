46da3b81b12a87d29baa803766af244b135bd290
Gan Tu <github@tugan.dev>
2020-06-07 20:01:28 -0700

    add disclaimers

--- src/benchmarks/write_bench.cc
+++ src/benchmarks/write_bench.cc
@@ -93,25 +93,31 @@ static void BM_WRITE_EXISTING(benchmark::State& state) {
   state.counters["failed"] = failed;
 }
 
-// Register the function as a benchmark
+// The benchmark may fail if you run all of them at the same time, because
+// the GFS server cluster may have a limit on its disk size, with a max number
+// of chunks available for allocation. Thus, when google benchmark run thousands 
+// of iterations, the GFS will quickly become unable to handle read/write
+// 
+// It can also fail due to change of random filename collide and we prohibit
+// re-createing the same filename. Thus, use with caution
 
-// BENCHMARK(BM_WRITE_NEW)
-//     ->Iterations(500)
-//     ->DenseRange(1, 10 * 1000 + 1, 1000);  // 1KB to 10MB
+BENCHMARK(BM_WRITE_NEW)
+    ->Iterations(500)
+    ->DenseRange(1, 10 * 1000 + 1, 1000);  // 1KB to 10MB
 
-// BENCHMARK(BM_WRITE_EXISTING)
-//     ->Iterations(500)
-//     ->DenseRange(1, 10 * 1000 + 1, 1000);  // 1KB to 10MB
+BENCHMARK(BM_WRITE_EXISTING)
+    ->Iterations(500)
+    ->DenseRange(1, 10 * 1000 + 1, 1000);  // 1KB to 10MB
 
-// BENCHMARK(BM_WRITE_NEW)
-//     ->Iterations(100)
-//     ->ThreadRange(/*min_threads=*/1, /*max_threads=*/100)
-//     ->Range(1, 1000);  // 1KB to 1MB
+BENCHMARK(BM_WRITE_NEW)
+    ->Iterations(100)
+    ->ThreadRange(/*min_threads=*/1, /*max_threads=*/100)
+    ->Range(1, 1000);  // 1KB to 1MB
 
-// BENCHMARK(BM_WRITE_EXISTING)
-//     ->Iterations(100)
-//     ->ThreadRange(/*min_threads=*/1, /*max_threads=*/100)
-//     ->Range(1, 1000);  // 1KB to 1MB
+BENCHMARK(BM_WRITE_EXISTING)
+    ->Iterations(100)
+    ->ThreadRange(/*min_threads=*/1, /*max_threads=*/100)
+    ->Range(1, 1000);  // 1KB to 1MB
 
 // Instead of using BENCHMARK_MAIN, we manually write the main to allows
 // use initialize the google logging, to surpress the INFO log being logged
